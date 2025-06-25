52cf35f3ed82c6b36e29dfd69cd9fa99814436d8
Doxteur <doxteurn@gmail.com>
2025-06-25 15:17:17 +0200

    first commit

---
+++ index.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const app = express();
+const fs = require('fs');
+const bodyParser = require('body-parser');
+app.use(bodyParser.urlencoded({ extended: true }));
+
+let tasks = [];
+
+app.get('/', (req, res) => {
+  res.send('<h1>Todo App</h1><form method="POST" action="/add-task"><input name="task"/><button>Add</button></form><ul>' +
+    tasks.map(t => `<li>${t}</li>`).join('') + '</ul>');
+});
+
+app.post('/add-task', (req, res) => {
+  const task = req.body.task; // pas de validation = faille XSS possible
+  tasks.push(task);
+  res.redirect('/');
+});
+
+app.listen(3000, () => console.log('Todo app running on http://localhost:3000'));
